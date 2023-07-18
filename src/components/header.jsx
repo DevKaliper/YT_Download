7b28c001fb097ecc8f90ada61f8a4b3651557d60
Dev Kaliper <gxlthz@gmail.com>
2023-07-17 23:19:53 -0500

    Header and welcome text done

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,16 +1,35 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
-    <nav className='sticky top-0 left-0 ' >
-      <div className=' text-white w-full bg-[#120d26] rounded-3xl  justify-between mt-5 flex -center md:w-[65%] h-[4rem] mx-auto px-8 py-4 backdrop-blur'>
-        <div className='ml-4'>logo</div>
-        <div className='mx-auto ' >sectiones</div>
-        <div className='mr-4'>socials</div>
-
+    <nav className="sticky left-0 top-0 ">
+      <div className="  mx-auto mt-5 flex  h-[4rem] w-full items-center justify-between rounded-3xl bg-[#120d26] px-8 py-4 text-white backdrop-blur md:w-[65%]">
+        <div className="ml-2 flex items-center justify-center gap-3">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="h-6 w-6">
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418"
+            />
+          </svg>
+          100% Free
+        </div>
+        <div className="mx-auto flex gap-6 text-lg text-purple-200 opacity-75  ">
+          <span>Home</span> <span>Audio</span> <span>Executable</span>
+        </div>
+        <div className="mr-4">
+          <a href="https://github.com/DevKaliper/YT_Download" className="flex gap-2 justify-center items-center py-2 px-3 rounded-lg relative hover:shadow-purple-300 after:w-full after:bg-purple-400 after:absolute after:-top-0  after:rounded-lg after:blur-sm after:h-full after:left-0 hover:shadow-inner duration-200 after:-z-20 bg-[#050211]"><span className="fill-neutral-100 "><svg xmlns="http://www.w3.org/2000/svg" height="1.6em" viewBox="0 0 496 512"><path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"/></svg></span><span className="text-lg">Github</span></a>
+      
+        </div>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
