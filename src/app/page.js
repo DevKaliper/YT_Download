42cab3ce39b10041e4017eecbf8adeea6318ba6a
Dev Kaliper <gxlthz@gmail.com>
2023-07-18 14:27:54 -0500

    logic created for backend server

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,5 @@
+import MoukUp from "@/components/MoukUp";
+import YouTubeDownloader from "../../backend/DownloaderProof";
 
 
 export default function Home() {
@@ -6,11 +8,15 @@ export default function Home() {
 <section>
   <div className="flex flex-col items-center min-h-screen py-2 px-2">
     <h1 className=" text-4xl md:text-6xl 2xl:text-7xl font-bold text-center text-white mt-10 md:mt-20">
-      Welcome to the <a className="bg-clip-text bg-gradient-to-r from-white to-purple-800 text-transparent" href="https://nextjs.org">Free YT Downloader</a>
+      Welcome to the <a className="bg-clip-text bg-gradient-to-r from-white to-purple-800 text-transparent" href="#">Free YT Downloader</a>
     </h1>
     <p className="text-white text-center mt-4 text-lg italic 2xl:text-2xl">
       You can download any video from YouTube for free. Just paste the link and download it. 
     </p>
+    <MoukUp />
+    
+    
+    
     
 
     
---
+++ src/components/MoukUp.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const MoukUp = () => {
+  return (
+    <div className="mockup-code w-1/2 shadow-2xl shadow-purple-500 mt-20">
+    <pre data-prefix="$"><code>npm i daisyui</code></pre> 
+    <pre data-prefix=">" className="text-warning mb-2"><code>Put the video's link here...</code></pre> 
+    <pre data-prefix=">" className="text-success"><input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" /></pre>
+  </div>
+  )
+}
+
+export default MoukUp
