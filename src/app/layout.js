1ce3488dfc3b3a6b76541f5904aba168c1e957bf
Dev Kaliper <gxlthz@gmail.com>
2023-07-25 19:37:44 -0500

    download page created

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,12 @@
 
 "use client"
 import './globals.css'
-import { Inter } from 'next/font/google'
+
 import Header from '@/components/header'
 import Footer from '@/components/footer'
 import {Ball} from '@/components/CursorShadow'
 import Head from 'next/head'
-const inter = Inter({ subsets: ['latin'] })
+
 
 
 export default function RootLayout({ children }) {
--- src/components/MoukUp.jsx
+++ src/components/MoukUp.jsx
@@ -1,18 +1,16 @@
 import { useState } from "react";
 import service from "../services/fetchVideo";
 
-
 const MoukUp = () => {
   const [url, setUrl] = useState("");
   const handleChange = (e, setSomething) => {
     setSomething(e.target.value);
-    
-  }
+  };
   const handleSubmit = (url) => {
-    service.fetchVideo(url).then(res => console.log(res.videoDetails.title))
-  }
+    service.fetchVideo(url).then((res) => console.log(res.videoDetails.title));
+  };
   return (
-    <div className="mockup-code relative mt-[5rem] w-full shadow-2xl shadow-purple-500 lg:w-1/2 overflow-visible">
+    <div className="mockup-code relative mt-[5rem] w-full overflow-visible shadow-2xl shadow-purple-500 lg:w-1/2">
       <pre data-prefix="$">
         <code>Paste your link down below</code>
       </pre>
@@ -32,7 +30,9 @@ const MoukUp = () => {
           className="input input-bordered w-full max-w-xs"
         />
       </pre>
-      <button onClick={()=>handleSubmit(url)} className="btn flex flex-row gap-2 justify-center items-center shadow-xl shadow-purple-800 absolute right-0 -bottom-8 md:-bottom-5 " >
+      <button
+        onClick={() => handleSubmit(url)}
+        className="btn absolute -bottom-8 right-0 flex flex-row items-center justify-center gap-2 shadow-xl shadow-purple-800 md:-bottom-5 ">
         <div>
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -44,11 +44,11 @@ const MoukUp = () => {
             <path
               strokeLinecap="round"
               strokeLinejoin="round"
-              d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"
+              d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
             />
           </svg>
-          
-        </div><span>Download</span>
+        </div>
+        <span>Search</span>
       </button>
     </div>
   );
