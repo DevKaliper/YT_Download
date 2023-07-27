becc6177a1c1ba14947938bd30b82009d61833b4
Dev Kaliper <gxlthz@gmail.com>
2023-07-27 16:57:39 -0500

    fixing typos

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import './globals.css'
 
 
 import Header from '@/components/header'
-import Footer from '@/components/footer'
+import Footer from '@/components/Footer'
 import {Ball} from '@/components/CursorShadow'
 import Head from 'next/head'
 
