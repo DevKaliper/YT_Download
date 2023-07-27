e364733eb453c2ff5a3080ef243ff73de74f199e
Dev Kaliper <gxlthz@gmail.com>
2023-07-27 13:56:46 -0500

    information for downloading video done

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,72 +1,86 @@
 import React from "react";
-import service from "../services/fetchVideo";
+import { closeSnackbar, enqueueSnackbar } from "notistack";
 function VideoCard({ data }) {
-  // const handleGetVideo = () => {
-  //   // FUNCION QUE ENVIA EL LINK A LA API PARA QUE SE DESCARGUE EL VIDEO
-  //   const url = data?.video_url;
-  //   service
-  //     .downloadVideo(url)
-  //     .then((res) => {
-  //       console.log("video descargado");
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //     });
-  // };
-
-
   const handleDownload = () => {
     const datas = {
       url: data?.video_url,
-      fileName:'video_descargado',
+      fileName: "video_descargado",
     };
+    enqueueSnackbar("Intentando descargar el video...", {
+      persist: true,
+      variant: "success",
+    });
 
-    fetch('http://localhost:5000/download', {
-      method: 'POST',
+    fetch("http://localhost:5000/download", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(datas),
     })
       .then((response) => {
-        console.log(response.blob)
+        closeSnackbar();
+        console.log(response.blob);
+
         // Generar la descarga en el navegador
         response.blob().then((blob) => {
-          console.log(blob)
+          console.log(blob);
           const url = window.URL.createObjectURL(new Blob([blob]));
-          const a = document.createElement('a');
+          const a = document.createElement("a");
           a.href = url;
-          a.download = `hola1.mp4`;
+          a.download = `${data?.title}.mp4`;
           a.click();
         });
       })
       .catch((error) => {
-        console.error('Error al realizar la solicitud:', error);
+        enqueueSnackbar(`Error al descargar el video: ${error.message}`, {
+          autoHideDuration: 3000,
+          variant: "error",
+        });
+        console.error("Error al realizar la solicitud:", error);
       });
   };
   return (
     <div className="min-w-screen my-5 grid min-h-[70vh]  place-items-center ">
-      <div className="card mx-auto h-auto w-96 bg-base-100 shadow-xl">
+      <div className="card mx-auto h-auto w-96 bg-purple-950 bg-opacity-75 shadow-2xl shadow-purple-800">
         <figure className="mt-5 ">
           <img
-            className="rounded-xl"
+            className="rounded-xl shadow-lg"
             src={data?.thumbnails[0]?.url}
             width={250}
             alt="video thumbnail"
           />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">
+          <h2 className="card-title  text-purple-200">
             {data?.title}
             <div className="badge badge-secondary">{data?.category}</div>
           </h2>
           <p className="overflow-hidden">
             {" "}
-            <strong className="text-lg underline">Description: </strong>
+            <strong className="text-lg  text-purple-400">Description: </strong>
             {data?.description}
           </p>
           <div className="card-actions justify-end">
-            <button className="btn my-5 " onClick={ handleDownload}>Download with highest quality</button>
+            <button
+              className="btn my-5 hover:bg-purple-700 flex bg-base gap-3 justify-center items-center shadow-lg shadow-gray-700"
+              onClick={handleDownload}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="h-6 w-6">
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"
+                />
+              </svg>
+
+              <span>Download with highest quality</span>
+            </button>
           </div>
         </div>
       </div>
